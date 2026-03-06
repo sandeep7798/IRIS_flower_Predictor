a325476f23254d284fcba9090f262967399e8b9a
sandeep7798 <salugusandeep92@gmail.com>
2026-03-06 23:59:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_iris
+import os
 
 app = Flask(__name__)
 
 iris = load_iris()
 
-model = load_model("iris_model.h5")
+model = load_model("model.keras")
 
 scaler = StandardScaler()
 scaler.fit(iris.data)
@@ -27,4 +28,5 @@ def predict():
     return render_template('index.html', prediction_text="Predicted class: {}".format(output))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
